3cbb947834086fd21cc972d49fb84f5a9f961ee9
Kimigabuchi <kirillraum@mail.ru>
2018-05-22 23:38:35 +0300

    create app. Step 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function HelloWorld() {
+  return <h1>Hello World!</h1>
+}
+
+render(<HelloWorld/>, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();

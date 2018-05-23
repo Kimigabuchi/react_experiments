7e68c48de3920af9d8fc7da2a836aa4975573f11
Kimigabuchi <kirillraum@mail.ru>
2018-05-24 00:30:41 +0300

    Lesson 1. 60min

---
+++ src/Article.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+export default class Article extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false,
+    }    
+  }
+  render() {
+    const {article} = this.props;
+    const {isOpen} = this.state;
+    return (
+      <div>
+        <h3>{article.title}</h3>
+        <button onClick={this.toggleOpen}>{this.getButtonDescription()}</button>
+        {this.getBody()}
+      </div>
+    )
+  }
+  toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+  getBody = () => {
+    const {article} = this.props;
+    return this.state.isOpen ? <section>{article.text}</section> : null;
+  }
+  getButtonDescription = () => {
+    return this.state.isOpen ? "close" : "open";
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReactDOM, {render} from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Article from './Article';
+import {articles} from './fixtures';
 
-function HelloWorld() {
-  return <h1>Hello World!</h1>
-}
+const article = articles[0];
 
-render(<HelloWorld/>, document.getElementById('root'));
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+render(<Article article={article} />, document.getElementById('root'));

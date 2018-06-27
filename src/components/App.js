ae9b24a070d5c6a0dd69570337d5eb3bcbfc8cbf
k.trifonov <k.trifonov@zonatelecom.ru>
2018-06-27 18:17:59 +0300

    Lesson 3. Homework. Task 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
+import DateInterval from './DateInterval'
 
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <UserForm />
+        <DateInterval />
         <Select options={options} value={this.state.selection} onChange={this.changeSelection} multi={true} />
         <ArticleList articles = {articles} defaultOpenID={articles[0].id} />
       </div>

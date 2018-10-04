3c4658e2a3700842cc43e27ab71b2a28eb94eca2
ktrifonov <k.trifonov@zonatelecom.ru>
2018-10-04 16:48:25 +0300

    Lesson 4. Hometask 4. Before check

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -20,7 +20,7 @@ class Article extends Component {
     const {article, isOpen, toggleOpen} = this.props;
     return (
       <div> 
-        <h3>{article.title}</h3>
+        <h3>{article.title} - {article.date}</h3>
         <button onClick={toggleOpen}>
           {this.getButtonDescription(isOpen)}
         </button>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,6 +3,7 @@ import Article from './Article';
 import accordeonOpen from '../decorators/accordeon';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {setSelectArticles} from '../AC'
 
 class ArticleList extends Component {
   static propTypes = {
@@ -15,14 +16,26 @@ class ArticleList extends Component {
     accordeonOpen: PropTypes.func,
   }
   render() {
-    const {articles, openItemID, accordeonOpen} = this.props;
-    const articleElements = articles.map((article) =>  <li key={article.id}>
-                                                                    <Article
-                                                                      article={article}
-                                                                      isOpen={article.id === openItemID}
-                                                                      toggleOpen={accordeonOpen(article.id)}
-                                                                    />
-                                                                  </li>);
+    const {articles, openItemID, accordeonOpen, selected, date} = this.props;
+    const articleElements = articles
+                              .filter(article => {
+                                if (!selected) return article
+                                if (selected.length === 0) return article
+                                return !!selected.find(select => select.value === article.id)
+                              })
+                              .filter(article => {
+                                const {to, from} = date
+                                const current = new Date(article.date)
+                                if (!to) return true
+                                return current >= from && current <= to
+                              })
+                              .map((article) => <li key={article.id}>
+                                                  <Article
+                                                    article={article}
+                                                    isOpen={article.id === openItemID}
+                                                    toggleOpen={accordeonOpen(article.id)}
+                                                  />
+                                                </li>);
     return (
       <ul>
         {articleElements}
@@ -31,6 +44,4 @@ class ArticleList extends Component {
   }
 }
 
-export default connect(state => ({
-  articles: state.articles
-}))(accordeonOpen(ArticleList));
+export default connect(({articles, selected, date}) => ({articles, selected, date}))(accordeonOpen(ArticleList));
